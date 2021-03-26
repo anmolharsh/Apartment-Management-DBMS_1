54ad386d2dbaf6c84f00e3a45c5a013511b14d31
ajaykumarr123 <ajaymeena3210@gmail.com>
2021-03-26 11:29:43 +0530

    merged acccounts and complaints app

--- Apartment_Management/accounts/admin.py
+++ Apartment_Management/accounts/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Resident,Watchmen,Profile
+
+admin.site.register(Resident)
+admin.site.register(Watchmen)
+admin.site.register(Profile)
