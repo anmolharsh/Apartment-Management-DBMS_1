ef40505021160abc8aac19e2ea1a9b77667777a0
anmolharsh <anmolharsh@iitkgp.ac.in>
2021-03-29 04:28:36 +0530

    buildings done

--- Apartment_Management/buildings/urls.py
+++ Apartment_Management/buildings/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
-app_name = 'accounts'
+app_name = 'buildings'
 
 urlpatterns = [
-    # path('view/<int:user_id>/',views.view_main, name='main_view'),
+    path('view/',views.view_main, name='main_view'),
    
     path('view/building/', views.view_building, name="building_details"),
     path('view/flat/',views.view_flat, name="flat_details"),
--- Apartment_Management/buildings/views.py
+++ Apartment_Management/buildings/views.py
@@ -4,51 +4,67 @@ from .models import Building, Flat, Service_Directory, Occupies
 # Create your views here.
 
 
-# def view_main(request):
+def view_main(request):
 	# model = Occupies.filter(user = request.user)
-	# render
+	cnt = 0
+	return render(request, 'buildings/view_main.html')
 
 
 def view_flat(request):
 	model = Occupies.objects.filter(user_id = request.user.pk)
-	print(request.user.pk)
 	flat_model = []
 	for i in model:
-		print(i.flat_id)
 		temp = Flat.objects.filter(flat_id = str(i.flat_id.pk))
 		flat_model.append(temp)
-	print(flat_model)
 	new_flat_model = []
-
 	for i in flat_model:
 		new_flat_model.append(i[0])
-	# print(Flat.objects.all())
 	return render(request, 'buildings/flat.html', {'flat_model':new_flat_model})
 
 
 def view_building(request):
-	# model = Occupies.objects.filter(user_id = request.user)
-	# for i in model:
-	# 	flat_model += Flat.objects.filter(flat_id = i.flat_id)
-	# building_model = Building.objects.filter(building_id = flat_model.building_id)
-	# # render
-	# return render(request, 'buildings/building.html', building_model)
-	cnt = 0
-	print(cnt)
+	model = Occupies.objects.filter(user_id = request.user.pk)
+	flat_model = []
+	for i in model:
+		temp = Flat.objects.filter(flat_id = str(i.flat_id.pk))
+		flat_model.append(temp)
+	new_flat_model = []
+	for i in flat_model:
+		new_flat_model.append(i[0])
+	building_model = []
+	for i in new_flat_model:
+		temp = Building.objects.filter(building_id = str(i.building_id.pk))
+		building_model.append(temp)
+	new_building_model = []
+	for i in building_model:
+		new_building_model.append(i[0])
+	new_building_model = set(new_building_model)
+	return render(request, 'buildings/building.html', {'flat_model':new_building_model})
+
 
 def view_service_directory(request):
-	# model = Occupies.objects.filter(user_id = request.user)
-	# flat_model = []
-	# for i in model:
-	# 	flat_model += Flat.objects.filter(flat_id = i.flat_id)
-	# service_directory_model = []
-	# for i in flat_model:
-	# 	service_directory_model += Service_Directory.objects.filter(building_id = i.building_id)
-	# # render
-	# print('\t\ttttttttttttttttttttttt\n', service_directory_model)
-	# return render(request, 'buildings/service_directory.html', service_directory_model)
-	cnt = 0
-	print(cnt)
+	model = Occupies.objects.filter(user_id = request.user.pk)
+	flat_model = []
+	for i in model:
+		temp = Flat.objects.filter(flat_id = str(i.flat_id.pk))
+		flat_model.append(temp)
+	new_flat_model = []
+	for i in flat_model:
+		new_flat_model.append(i[0])
+	building_model = []
+	for i in new_flat_model:
+		temp = Building.objects.filter(building_id = str(i.building_id.pk))
+		building_model.append(temp)
+	new_building_model = []
+	for i in building_model:
+		new_building_model.append(i[0])
+	new_building_model = set(new_building_model)
+	sd = []
+	for i in new_building_model:
+		temp = Service_Directory.objects.filter(building_id = i.building_id)
+		sd += temp
+	new_sd = sd
+	return render(request, 'buildings/service_directory.html', {'flat_model':new_sd})
 
 
 
