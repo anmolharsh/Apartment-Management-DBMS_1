426944ff9c523049348684a75b5c3f1b418d015c
anmolharsh <anmolharsh@iitkgp.ac.in>
2021-03-29 01:47:09 +0530

    buildings:flat/no_display

--- Apartment_Management/buildings/views.py
+++ Apartment_Management/buildings/views.py
@@ -10,18 +10,24 @@ from .models import Building, Flat, Service_Directory, Occupies
 
 
 def view_flat(request):
-	# model = Occupies.objects.filter(user_id = 1)
-	# flat_model = []
-	# for i in model:
-	# 	flat_model += Flat.objects.filter(flat_id = i.flat_id)
+	model = Occupies.objects.filter(user_id = request.user)
+	flat_model = []
+	for i in model:
+		print(i.flat_id)
+		temp = Flat.objects.filter(flat_id = i.flat_id)
+		flat_model.append(temp)
+		print(Flat.objects.filter(flat_id = i.flat_id))
 	# # render
+	# flat_model += Flat.objects.filter(flat_id = model.flat_id)
 	cnt = 0
 	new_dict = []
-	# for i in flat_model:
+	for i in flat_model:
+		for j in i:
+			print("t\t",j)
 	# 	new_dict = {cnt:i}
 	# 	cnt+=1
-	print(cnt)
-	return render(request, 'buildings/flat.html', {'new_dict':new_dict})
+	print(flat_model)
+	return render(request, 'buildings/flat.html', {'flat_model':flat_model})
 
 
 def view_building(request):
